30e050c3827bd8443015bd98c2c1b4755ff389c2
Rafael López Martínez <rafael.lopez.martinez@sap.com>
2022-07-11 10:11:28 +0200

    using req.user api

--- srv/users.service.js
+++ srv/users.service.js
@@ -1,6 +1,8 @@
 module.exports = (srv) => {
     srv.before ('READ','*', (req)=>{
         console.log(req.user)
+        console.log(req.user.id)
+        console.log(req.user.attr)
     })
 
     srv.on('getCurrentUserID', (req) => {
